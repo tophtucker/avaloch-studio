4eb97f07bea63f2fa88d049a0937ec3f5a41910c
Toph Tucker <tophtucker@gmail.com>
2025-09-06 20:12:10 -0400

    add special hours overrides and explicit closed toggle, claire signs off on it

--- schemaTypes/restaurantType.ts
+++ schemaTypes/restaurantType.ts
@@ -1,4 +1,18 @@
+import {ConditionalProperty} from 'sanity'
+import {StringRule} from 'sanity'
 import {defineField, defineType} from 'sanity'
+
+const timeValidator = (rule: StringRule) =>
+  rule.custom((value) => {
+    if (value === undefined) return true
+    const test = /^(?:[01]\d|2[0-3]):[0-5]\d|24:00$/.test(value)
+    if (!test) {
+      return 'Must be a 24-hour time formatted like “09:00” (for 9 AM) or “22:30” (for 10:30 PM).'
+    }
+    return true
+  })
+
+const disableIfClosed: ConditionalProperty = ({parent}) => !!(parent && parent.closed)
 
 export const restaurantType = defineType({
   name: 'restaurant',
@@ -45,29 +59,69 @@ export const restaurantType = defineType({
             title,
             type: 'object',
             fields: [
+              defineField({type: 'boolean', name: 'closed', title: 'Closed'}),
               defineField({
                 name: 'open',
                 title: 'Open time',
                 type: 'string',
-                validation: (rule) =>
-                  rule.custom((value, context) => {
-                    if (value === undefined) return true
-                    const test = /^(?:[01]\d|2[0-3]):[0-5]\d|24:00$/.test(value)
-                    if (!test) {
-                      return 'Must be a 24-hour time formatted like “09:00” (for 9 AM) or “22:30” (for 10:30 PM).'
-                    }
-                    return true
-                  }),
+                validation: timeValidator,
+                readOnly: disableIfClosed,
               }),
               defineField({
                 name: 'close',
                 title: 'Close time',
                 type: 'string',
+                validation: timeValidator,
+                readOnly: disableIfClosed,
               }),
             ],
-            options: {columns: 2},
+            options: {columns: 3},
           }),
       ),
+    }),
+    defineField({
+      name: 'hourOverrides',
+      title: 'Special hours',
+      description:
+        'Special hours override default weekly hours. For example, you could say the restaurant is closed on Christmas.',
+      type: 'array',
+      of: [
+        defineField({
+          type: 'object',
+          name: 'override',
+          fields: [
+            defineField({type: 'date', name: 'date', title: 'Date'}),
+            defineField({type: 'boolean', name: 'closed', title: 'Closed'}),
+            defineField({
+              type: 'string',
+              name: 'open',
+              title: 'Open time',
+              validation: timeValidator,
+              readOnly: disableIfClosed,
+            }),
+            defineField({
+              type: 'string',
+              name: 'close',
+              title: 'Close time',
+              validation: timeValidator,
+              readOnly: disableIfClosed,
+            }),
+          ],
+          preview: {
+            select: {
+              date: 'date',
+              open: 'open',
+              close: 'close',
+              closed: 'closed',
+            },
+            prepare({date, open, close, closed}) {
+              return {
+                title: `${date}: ${closed ? 'Closed' : `${open} – ${close}`}`,
+              }
+            },
+          },
+        }),
+      ],
     }),
   ],
 })
